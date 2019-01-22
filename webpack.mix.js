c9f7d479fffaa3f648c41859ac58afc1e9ef1333
Vitaly <vitalykk1983@gmail.com>
2019-01-23 00:17:25 +0200

    Add Route Admin & view dashboard

--- webpack.mix.js
+++ webpack.mix.js
@@ -40,3 +40,4 @@ mix.scripts([
 
 mix.copy('resources/assets/admin/bootstrap/fonts', 'public/fonts');
 mix.copy('resources/assets/admin/dist/fonts', 'public/fonts');
+mix.copy('resources/assets/admin/dist/img', 'public/img');
